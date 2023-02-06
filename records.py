018dc8ed857ea4f47c3cf6fb377ce83fc8c72f62
Victor Chen <victorc.1@outlook.com>
2023-02-06 01:13:13 -0500

    rework records model, implement record creation fn
    Use a new data model that separates registration form responses from
    verified participants, mentors, and judges.
    Add methods to create and check registration form response records and
    verified participant/mentor/judge records.

--- records.py
+++ records.py
@@ -1,204 +1,178 @@
 import os
 import sqlite3
 
-_DATABASE_FILE = "records.db"
+_DATABASE_FILE = 'records.db'
+
+_PARTICIPANT_REG_RESPONSES_TABLE_NAME = 'participant_reg_responses'
+_MENTOR_REG_RESPONSES_TABLE_NAME = 'judge_reg_responses'
+_JUDGE_REG_RESPONSES_TABLE_NAME = 'judge_reg_responses'
+
+_PARTICIPANT_TABLE_NAME = 'participants'
+_MENTOR_TABLE_NAME = 'mentors'
+_JUDGE_TABLE_NAME = 'judges'
+
+
+def _initialize_db(cursor: sqlite3.Cursor):
+    # Registration form responses
+    cursor.execute(
+        f'CREATE TABLE {_PARTICIPANT_REG_RESPONSES_TABLE_NAME} ( email TEXT NOT NULL, discord_tag TEXT NOT NULL )')
+    cursor.execute(
+        f'CREATE TABLE {_MENTOR_REG_RESPONSES_TABLE_NAME} ( email TEXT NOT NULL, discord_tag TEXT NOT NULL )')
+    cursor.execute(
+        f'CREATE TABLE {_JUDGE_REG_RESPONSES_TABLE_NAME} ( email TEXT NOT NULL, discord_tag TEXT NOT NULL )')
+
+    # Verified users
+    cursor.execute(
+        f'CREATE TABLE {_PARTICIPANT_TABLE_NAME} ( discord_id INTEGER PRIMARY KEY, email TEXT OT NULL, team_id REFERENCES teams(id) )')
+    cursor.execute(
+        f'CREATE TABLE {_MENTOR_TABLE_NAME} ( discord_id INTEGER PRIMARY KEY, email TEXT NOT NULL )')
+    cursor.execute(
+        f'CREATE TABLE {_JUDGE_TABLE_NAME} ( discord_id INTEGER PRIMARY KEY, email TEXT NOT NULL )')
+
+    # Teams
+    cursor.execute('CREATE TABLE teams ( id INTEGER PRIMARY KEY AUTOINCREMENT, name TEXT UNIQUE NOT NULL, category_channel_id INTEGER NOT NULL, text_channel_id INTEGER NOT NULL, voice_channel_id INTEGER NOT NULL, role_id INTEGER NOT NULL )')
+
+
+def add_participant_response_entry(email: str, discord_tag: str):
+    """Add a participant registration response entry to the records.
+
+    Args:
+        email (str): The email address for the entry to add
+        discord_tag (str): The Discord tahg for the entry to add
+    """
+
+    _cursor.execute('INSERT INTO {_PARTICIPANT_REG_RESPONSES_TABLE_NAME} ( email, discord_tag ) VALUES ( :email, :discord_tag )', {
+                    'email': email, 'discord_tag': discord_tag})
+
+
+def add_mentor_response_entry(email: str, discord_tag: str):
+    """Add a mentor registration response entry to the records.
+
+    Args:
+        email (str): The email address for the entry to add
+        discord_tag (str): The Discord tahg for the entry to add
+    """
+
+    _cursor.execute('INSERT INTO {_MENTOR_REG_RESPONSES_TABLE_NAME} ( email, discord_tag ) VALUES ( :email, :discord_tag )', {
+                    'email': email, 'discord_tag': discord_tag})
+
+
+def add_judge_response_entry(email: str, discord_tag: str):
+    """Add a judge registration response entry to the records.
+
+    Args:
+        email (str): The email address for the entry to add
+        discord_tag (str): The Discord tahg for the entry to add
+    """
+
+    _cursor.execute('INSERT INTO {_JUDGE_REG_RESPONSES_TABLE_NAME} ( email, discord_tag ) VALUES ( :email, :discord_tag )', {
+                    'email': email, 'discord_tag': discord_tag})
+
+
+def participant_response_exists(email: str, discord_tag: str) -> bool:
+    """Check if there exists a participant registration response entry with the
+    given email address and Discord tag.
+
+    Args:
+        email (str): The email address
+        discord_tag (str): The Discord tag
+
+    Returns:
+        bool: If there is a record with the given email address and Discord tag
+    """
+
+    return _cursor.execute('SELECT COUNT(*) FROM {_PARTICIPANT_REG_RESPONSES_TABLE_NAME} WHERE email=:email AND discord_tag=:discord_tag', {
+        'email': email, 'discord_tag': discord_tag}).fetchone()[0] > 0
+
+
+def mentor_response_exists(email: str, discord_tag: str) -> bool:
+    """Check if there exists a mentor registration response entry with the
+    given email address and Discord tag.
+
+    Args:
+        email (str): The email address
+        discord_tag (str): The Discord tag
+
+    Returns:
+        bool: If there is a record with the given email address and Discord tag
+    """
+
+    return _cursor.execute('SELECT COUNT(*) FROM {_MENTOR_REG_RESPONSES_TABLE_NAME} WHERE email=:email AND discord_tag=:discord_tag', {
+        'email': email, 'discord_tag': discord_tag}).fetchone()[0] > 0
+
+
+def judge_response_exists(email: str, discord_tag: str) -> bool:
+    """Check if there exists a judge registration response entry with the
+    given email address and Discord tag.
+
+    Args:
+        email (str): The email address
+        discord_tag (str): The Discord tag
+
+    Returns:
+        bool: If there is a record with the given email address and Discord tag
+    """
+
+    return _cursor.execute('SELECT COUNT(*) FROM {_JUDGE_REG_RESPONSES_TABLE_NAME} WHERE email=:email AND discord_tag=:discord_tag', {
+        'email': email, 'discord_tag': discord_tag}).fetchone()[0] > 0
+
+
+def add_participant(discord_id: int, email: str):
+    """Add a record for a verified participant.
+
+    There must not already exist a verified participant record with the same
+    Discord ID.
+
+    Args:
+        discord_id (int): Discord ID of the participant
+        email (str): Email address of the participant
+    """
+
+    _cursor.execute(f'INSERT INTO {_PARTICIPANT_TABLE_NAME} ( discord_id, email ) VALUES ( :discord_id, :email ) ', {
+        'discord_id': discord_id, 'email': email
+    })
+
+
+def add_mentor(discord_id: int, email: str):
+    """Add a record for a verified mentor.
+
+    There must not already exist a verified mentor record with the same Discord
+    ID.
+
+    Args:
+        discord_id (int): Discord ID of the mentor
+        email (str): Email address of the mentor
+    """
+
+    _cursor.execute(f'INSERT INTO {_MENTOR_TABLE_NAME} ( discord_id, email ) VALUES ( :discord_id, :email ) ', {
+        'discord_id': discord_id, 'email': email
+    })
+
+
+def add_judge(discord_id: int, email: str):
+    """Add a record for a verified judge.
+
+    There must not already exist a verified judge record with the same Discord
+    ID.
+
+    Args:
+        discord_id (int): Discord ID of the judge
+        email (str): Email address of the judge
+    """
+
+    _cursor.execute(f'INSERT INTO {_JUDGE_TABLE_NAME} ( discord_id, email ) VALUES ( :discord_id, :email ) ', {
+        'discord_id': discord_id, 'email': email
+    })
+
 
 _db_file_exists = os.path.isfile(_DATABASE_FILE)
-_connection = sqlite3.connect(_DATABASE_FILE)
+_connection = sqlite3.connect(_DATABASE_FILE, isolation_level=None)
 _cursor = _connection.cursor()
 
 if not _db_file_exists:
-    _cursor.execute(
-        "CREATE TABLE participants ( id INTEGER PRIMARY KEY AUTOINCREMENT, email, discord_tag, discord_id, team_id REFERENCES teams(id) )")
-    _cursor.execute(
-        "CREATE TABLE teams ( id INTEGER PRIMARY KEY AUTOINCREMENT, name, role_id, channel_category_id, channel_text_id, channel_voice_id )")
+    _initialize_db(_cursor)
 
-class Participant:
-    """Represents a participant record."""
-
-    def __init__(self, record):
-        self._id, self._email, self._discord_tag, self._discord_id, self._team_id = record
-
-    def get_by_record_id(record_id):
-        """Retrieves the participant with a matching record ID
-
-        Parameters:
-            record_id: record ID to match
-
-        Returns:
-            a Participant associated with matching record ID
-        """
-        return _cursor.execute("SELECT * FROM participants WHERE id=:id", {"id": record_id}).fetchone()
-
-    def get_by_discord_id(discord_id):
-        """Retrieves the participant with a matching discord ID
-
-        Parameters:
-            discord_id: discord ID address to match
-
-        Returns:
-            a Participant associated with the record with the matching discord
-            ID, or None if no such record exists
-        """
-        retrieved_record = _cursor.execute(
-            "SELECT * FROM participants WHERE discord_id=:id", {"id": discord_id}).fetchone()
-        return None if retrieved_record is None else Participant(retrieved_record)
-
-    def get_by_email(email):
-        """Retrieves the participant with a matching email
-
-        Parameters:
-            email: email address to match
-
-        Returns:
-            a Participant associated with the record with the matching email, or
-            None if no such record exists
-        """
-        retrieved_record = _cursor.execute(
-            "SELECT * FROM participants WHERE email=:email", {"email": email}).fetchone()
-        return None if retrieved_record is None else Participant(retrieved_record)
-
-    def get_by_discord_tag(discord_tag):
-        """Retrieves the participant with a matching Discord tag 
-
-        Parameters:
-            discord_tag: Discord tag to match
-
-        Returns:
-            a Participant associated with the record with the matching Discord
-            tag, or None if no such record exists
-        """
-        retrieved_record = _cursor.execute(
-            "SELECT * FROM participants WHERE discord_tag=:discord_tag", {"discord_tag": discord_tag}).fetchone()
-        return None if retrieved_record is None else Participant(retrieved_record)
-
-    def insert_record(email, discord_tag):
-        """Insert a new participant record, updating an existing record if one
-        with a matching email exists and has not been verified.
-
-        Parameters:
-            email: email address of participant
-            discord_tag: discord tag of participant
-
-        Returns:
-            a Participant associated with this record
-        """
-
-        participant = Participant.get_by_email(email)
-
-        if participant is None:
-            _cursor.execute("INSERT INTO participants ( email, discord_tag ) VALUES (:email, :discord_tag)", {
-                            "email": email, "discord_tag": discord_tag})
-            _connection.commit()
-            participant = Participant(_cursor.execute(
-                "SELECT * FROM participants WHERE id=:id", {"id": _cursor.lastrowid}).fetchone())
-        elif not participant.verified and participant.discord_tag != discord_tag:
-            participant.discord_tag = discord_tag
-
-        return participant
-
-    @property
-    def discord_tag(self):
-        return self._discord_tag
-
-    @discord_tag.setter
-    def discord_tag(self, value):
-        self._discord_tag = value
-        _cursor.execute("UPDATE participants SET discord_tag=:discord_tag WHERE id=:id", {
-                        "discord_tag": value, "id": self._id})
-        _connection.commit()
-
-    @property
-    def team_id(self):
-        return self._team_id
-
-    @team_id.setter
-    def team_id(self, value):
-        self._team_id = value
-
-        if value is None:
-            _cursor.execute(
-                "UPDATE participants SET team_id=NULL WHERE id=:id", {"id": self._id})
-        else:
-            _cursor.execute("UPDATE participants SET team_id=:team_id WHERE id=:id", {
-                            "team_id": value, "id": self._id})
-
-        _connection.commit()
-
-    @property
-    def discord_id(self):
-        return self._discord_id
-
-    @discord_id.setter
-    def discord_id(self, value):
-        self._discord_id = value
-        _cursor.execute("UPDATE participants SET discord_id=:discord_id WHERE id=:id", {
-                        "discord_id": value, "id": self._id})
-        _connection.commit()
-
-    @property
-    def verified(self):
-        return self.discord_id is not None
-
-    @property
-    def teamed(self):
-        return self.team_id is not None
-
-
-class Team:
-    def __init__(self, record):
-        self._id, self._name, self._role_id, self._channel_category_id, self._channel_text_id, self._channel_voice_id = record
-
-    def get_by_id(id):
-        return _cursor.execute("SELECT * FROM teams WHERE id=:id", {"id": id}).fetchone()
-
-    def create(name, role_id, channel_category_id, channel_text_id, channel_voice_id):
-        _cursor.execute("INSERT INTO teams ( name, role_id, channel_category_id, channel_text_id, channel_voice_id ) VALUES ( :name, :role_id, :channel_category_id, :channel_text_id, :channel_voice_id )", {
-                        "name": name, "role_id": role_id, "channel_category_id": channel_category_id, "channel_text_id": channel_text_id, "channel_voice_id": channel_voice_id})
-        _connection.commit()
-        return Team(_cursor.execute("SELECT * FROM teams WHERE id=:id", {"id": _cursor.lastrowid}).fetchone())
-
-    def drop(id):
-        _cursor.execute("DELETE FROM teams WHERE id=:id", {"id": id})
-        _connection.commit()
-
-    def add_participant(self, participant):
-        participant.team_id = self._id
-
-    def drop_participant(self, participant):
-        participant.team_id = None
-
-    @property
-    def size(self):
-        return len(_cursor.execute("SELECT id FROM participants WHERE team_id=:team_id", {"team_id": self._id}).fetchall())
-
-    @property
-    def participants(self):
-        return map(Participant.get_by_id, _cursor.execute("SELECT id FROM participants WHERE team_id=:team_id", {"team_id": self._id}).fetchall())
-
-    @property
-    def name(self):
-        return self._name
-
-    @name.setter
-    def name(self, value):
-        self._name = value
-        _cursor.execute("UPDATE teams SET name=:name WHERE id=:id", {
-                        "name": value, "id": self._id})
-        _connection.commit()
-
-    @property
-    def role_id(self):
-        return self._role_id
-
-    @property
-    def channel_category_id(self):
-        return self._channel_category_id
-
-    @property
-    def channel_text_id(self):
-        return self._channel_text_id
-
-    @property
-    def channel_voice_id(self):
-        return self._channel_voice_id
+print('Hello!')
+add_participant_response_entry('chen.10604@osu.edu', 'V⁰⁴#6118')
+# print(participant_response_exists('chen.10604@osu.edu', 'V⁰⁴#6118'))
+print('Goodbye!')
